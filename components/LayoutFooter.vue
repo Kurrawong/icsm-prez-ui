3ad0aaf8b7305cc9fa32142262bef39cc157bd11
jamiefeiss <jamie.feiss@gmail.com>
2025-03-10 16:13:03 +1000

    Initial ICSM theme from 4.0.1 Prez UI release

---
+++ components/LayoutFooter.vue
@@ -0,0 +1,158 @@
+<script lang="ts" setup>
+type NavLink = { label: string; url: string };
+
+const footerLinks: { title: NavLink; links: NavLink[] }[] = [
+    {
+        title: {
+            label: "About",
+            url: "https://www.icsm.gov.au/about",
+        },
+        links: [
+            {
+                label: "Our Role",
+                url: "https://www.icsm.gov.au/about/our-role",
+            },
+            {
+                label: "Communication",
+                url: "https://www.icsm.gov.au/about/communication",
+            },
+            {
+                label: "Strategic plan",
+                url: "https://www.icsm.gov.au/about/strategic-plan",
+            },
+            {
+                label: "Copyright",
+                url: "https://www.icsm.gov.au/about/legal",
+            },
+            {
+                label: "Disclaimer",
+                url: "https://www.icsm.gov.au/node/45",
+            },
+            {
+                label: "Privacy",
+                url: "https://www.icsm.gov.au/node/64",
+            },
+            {
+                label: "MEMBERS Section",
+                url: "https://www.icsm.gov.au/members",
+            },
+        ],
+    },
+    {
+        title: {
+            label: "What we do",
+            url: "https://www.icsm.gov.au/what-we-do",
+        },
+        links: [
+            {
+                label: "Addressing",
+                url: "https://www.icsm.gov.au/what-we-do/addressing",
+            },
+            {
+                label: "Cadastre",
+                url: "https://www.icsm.gov.au/what-we-do/cadastre-working-group",
+            },
+            {
+                label: "Elevation Depth & Imagery",
+                url: "https://www.icsm.gov.au/what-we-do/elevation-depth-and-imagery-working-group",
+            },
+            {
+                label: "Geodesy",
+                url: "https://www.icsm.gov.au/what-we-do/geodesy",
+            },
+            {
+                label: "Metadata",
+                url: "https://www.icsm.gov.au/what-we-do/metadata-working-group",
+            },
+            {
+                label: "S-100WG",
+                url: "https://www.icsm.gov.au/s-100-working-group",
+            },
+            {
+                label: "NZ S-100WG",
+                url: "https://www.icsm.gov.au/nz-s-100-working-group",
+            },
+            {
+                label: "Place Names Working Group",
+                url: "https://www.icsm.gov.au/what-we-do/place-names-working-group",
+            },
+            {
+                label: "Topographic Information",
+                url: "https://www.icsm.gov.au/what-we-do/topographic-information",
+            },
+            {
+                label: "Tides and Mean Sea Level",
+                url: "https://www.icsm.gov.au/what-we-do/tides-and-sea-level",
+            },
+        ],
+    },
+    {
+        title: {
+            label: "Education",
+            url: "https://www.icsm.gov.au/education",
+        },
+        links: [
+            {
+                label: "Fundamentals of Mapping",
+                url: "https://www.icsm.gov.au/education/fundamentals-mapping",
+            },
+            {
+                label: "Fundamentals of Land Ownership",
+                url: "https://www.icsm.gov.au/education/fundamentals-land-ownership-land-boundaries-and-surveying",
+            },
+            {
+                label: "Australian Tides Manual",
+                url: "https://www.icsm.gov.au/publications/australian-tides-manual-v50",
+            },
+            {
+                label: "Gazetteer of Australia",
+                url: "https://www.icsm.gov.au/education/gazetteer-australia",
+            },
+            {
+                label: "Upgrades to the Australian Geospatial Reference System",
+                url: "https://www.icsm.gov.au/upgrades-australian-geospatial-reference-system",
+            },
+
+        ],
+    },
+    {
+        title: {
+            label: "Publications",
+            url: "https://www.icsm.gov.au/publications",
+        },
+        links: [
+            {
+                label: "Australian Tides Manual",
+                url: "https://www.icsm.gov.au/publications/australian-tides-manual-v50",
+            },
+            {
+                label: "Control Surveys (SP1)",
+                url: "https://www.icsm.gov.au/standard-australian-survey-control-network-special-publication-1-sp1",
+            },
+            {
+                label: "GDA2020 Technical Manual",
+                url: "https://www.icsm.gov.au/gda2020-and-gda94-technical-manuals",
+            },
+        ],
+    },
+];
+</script>
+
+<template>
+    <footer class="bg-tertiary text-tertiary-foreground pt-6 pb-10">
+        <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            <div v-for="col in footerLinks">
+                <h3><a :href="col.title.url" target="_blank" rel="noopener noreferrer">{{ col.title.label }}</a></h3>
+                <div class="flex flex-col">
+                    <a v-for="link in col.links" :href="link.url" target="_blank" rel="noopener noreferrer">{{ link.label }}</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+footer a {
+    color: hsl(var(--tertiary-foreground)) !important;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { ChevronRight, ChevronLeft } from "lucide-vue-next";
+
+const props = defineProps<{
+    sidepanel?: boolean;
+    contentonly?: boolean;
+}>();
+const runtimeConfig = useRuntimeConfig();
+const expandSidePanel = ref(false);
+const showDebugPanel = ref(false);
+
+onBeforeMount(() => {
+    if (typeof localStorage !== 'undefined') {
+        expandSidePanel.value = !!localStorage.getItem('expandSidePanel');
+        showDebugPanel.value = runtimeConfig.public.prezDebug && !!localStorage.getItem('debug');
+        watch(expandSidePanel, val => localStorage.setItem('expandSidePanel', val && '1' || ''));
+        watch(showDebugPanel, val => localStorage.setItem('debug', val && '1' || ''));
+    }
+});
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen">
+        <div
+            class="absolute w-full -z-10 after:bg-gradient-to-b after:from-tertiary after:via-[rgba(0,27,68,.2)] via-40% after:to-[#f5f5f5] after:content-[' '] after:h-full after:w-full after:block after:absolute after:bottom-0 after:z-50">
+            <img src="/img/map.jpg" alt="" class="w-full">
+        </div>
+
+        <LayoutHeader />
+
+        <LayoutNav v-model="showDebugPanel" />
+
+        <!-- page heading -->
+        <slot v-if="!contentonly" name="header">
+            <div class="">
+                <div class="container mx-auto flex flex-row">
+                    <div class="px-4 py-4 flex-grow text-white">
+                        <slot name="breadcrumb" />
+                    </div>
+                    <div v-if="showDebugPanel" class="m-2 bg-gray-200 rounded-lg text-[12px] leading-[12px]">
+                        <slot name="debug" />
+                    </div>
+                </div>
+            </div>
+        </slot>
+        <div v-else-if="showDebugPanel" class="bg-gray-100">
+            <div class="container px-4 py-4 mx-auto">
+                <slot name="debug" />
+            </div>
+        </div>
+
+        <!-- content -->
+        <div class="container mx-auto flex-grow bg-background/85 rounded-md px-20 py-16">
+            <h1 v-if="!contentonly" class="text-3xl mt-4 mb-6">
+                <slot name="header-text" />
+            </h1>
+            <div v-if="sidepanel" class="grid grid-cols-4 gap-4">
+                <div :class="expandSidePanel ? 'col-span-3 relative' : 'col-span-4 relative'">
+                    <slot />
+                    <Button v-if="!expandSidePanel" title="Show sidepanel" variant="ghost" size="icon"
+                        class="absolute right-0 top-[-5px] pointer-events-auto"
+                        @click="expandSidePanel = !expandSidePanel">
+                        <ChevronLeft class="size-4" />
+                    </Button>
+                </div>
+                <div v-if="expandSidePanel" class="relative">
+                    <slot name="sidepanel" />
+                    <Button title="Hide sidepanel" variant="ghost" size="icon"
+                        class="absolute right-0 top-[-5px] pointer-events-auto"
+                        @click="expandSidePanel = !expandSidePanel">
+                        <ChevronRight class="size-4" />
+                    </Button>
+                </div>
+            </div>
+            <div v-else class="">
+                <slot />
+            </div>
+        </div>
+
+        <LayoutFooter />
+    </div>
+</template>
